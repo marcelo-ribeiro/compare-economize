84b48c2c2ac9cca293b8aa654ada989b5973b105
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-03-24 20:44:35 -0300

    Add pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,49 +1,7 @@
-import Head from "next/head";
 import "../styles/global.css";
 import "../styles/modal.css";
 import "../styles/utils.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="comparar preço" />
-        <title>Compare Economize</title>
-
-        <meta name="theme-color" content="#317EFB" />
-
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="comparar preço" />
+          <title>Compare Economize</title>
+
+          <meta name="theme-color" content="#317EFB" />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
