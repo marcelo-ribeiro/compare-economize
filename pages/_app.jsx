9ca037f1984f80146c0acbe56ad67a441be1aa77
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-03-24 20:06:21 -0300

    Add pwa

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,11 +10,13 @@ export default function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
         />
         <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta name="keywords" content="comparar preço" />
         <title>Compare Economize</title>
+
+        <meta name="theme-color" content="#317EFB" />
 
         <link rel="manifest" href="/manifest.json" />
         <link
@@ -29,9 +31,11 @@ export default function MyApp({ Component, pageProps }) {
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
